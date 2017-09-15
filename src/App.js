fb1dcce7de30d8ef4da4b3b2b8f0ef5e06119cd1
Samuel Villavicencio <samuelrey010@gmail.com>
2017-09-15 15:30:47 -0700

    Define necessary components, create static UI.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,94 @@
 import React, { Component } from 'react';
+import data from './data.json'
 import './App.css';
 
+class TrackSelectorButton extends Component {
+    render() {
+        let className = 'btn-primary';
+
+        return (
+            <button className={className}>
+                {this.props.track}
+            </button>
+        );
+    }
+}
+
+class InstrumentName extends Component {
+    render() {
+        return (
+            <h3>
+                {this.props.name}
+            </h3>
+        )
+    }
+}
+
+class InstrumentCard extends Component {
+    createButton(track) {
+        return (
+            <div>
+                <TrackSelectorButton track={track} />
+            </div>
+        )
+    }
+
+    render() {
+        let tracks = this.props.tracks;
+
+        return (
+            <div>
+                <InstrumentName name={this.props.instrument} />
+                {tracks.map(this.createButton)}
+            </div>
+        )
+    }
+}
+
+class InstrumentTable extends Component {
+    createCard(instrument, tracks) {
+        return (
+            <td>
+                <InstrumentCard instrument={instrument} tracks={tracks} />
+            </td>
+        )
+    }
+
+    render() {
+        let data = this.props.data;
+
+        return (
+            <table className='table'>
+                <td>
+                    <InstrumentCard instrument='drums' tracks={data.drums.tracks} />
+                </td>
+                <td>
+                    <InstrumentCard instrument='bass' tracks={data.bass.tracks} />
+                </td>
+            </table>
+        )
+    }
+}
+
+class AppHeader extends Component {
+    render() {
+        return (
+            <div className='App-header'>
+                <h2>{this.props.text}</h2>
+            </div>
+        )
+    }
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>This is Beat Burrito</h2>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='App'>
+                <AppHeader text='Beat Burrito'/>
+                <InstrumentTable data={data}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Card} from './Button'
-import data from './data.json'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Card title='drums' tracks={data.drums.tracks} />, document.getElementById('card'));
-ReactDOM.render(<Card title='basslines' tracks={data.bass.tracks} />, document.getElementById('card2'));
 registerServiceWorker();
